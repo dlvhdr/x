320f1169b8e8e2b2094ebf9e0bed44dbc217ef38
Dolev Hadar <dolevc2@gmail.com>
2025-11-14 19:40:27 +0200

    feat(gh-checks): init

--- gh-checks/gh-checks.go
+++ gh-checks/gh-checks.go
@@ -1,9 +1,20 @@
 // Package checks provides utlities for GitHub Actions checks
 package checks
 
-import "fmt"
+// CommitState is the status check rollup of a commit on GitHub.
+type CommitState string
 
-// PrintHelloWorld literally prints hello world.
-func PrintHelloWorld() {
-	fmt.Println("Hello, world!")
-}
+const (
+	// CommitStateExpected indicates we are still waiting for the checks to start.
+	CommitStateExpected CommitState = "EXPECTED"
+	// CommitStateError indicates some error occurred when running the checks.
+	CommitStateError CommitState = "ERROR"
+	// CommitStateFailure indicates a failure.
+	CommitStateFailure CommitState = "FAILURE"
+	// CommitStatePending indicates some check is still pending.
+	CommitStatePending CommitState = "PENDING"
+	// CommitStateSuccess indicates that overall the checks have been successful.
+	CommitStateSuccess CommitState = "SUCCESS"
+	// CommitStateUnknown indicates that that the state is not known.
+	CommitStateUnknown CommitState = "UNKNOWN"
+)
